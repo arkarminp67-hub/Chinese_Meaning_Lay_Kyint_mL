1c967fa4c2d24ddcecc89b28efa4fbe59bfe40c3
arkarminp67-hub <arkarmin.p67@rsu.ac.th>
2026-08-27 00:02:33 +0700

    Update data.js

--- data.js
+++ data.js
@@ -1,107 +1,230 @@
 const WORD_DATA = [
+
     {
-        chinese: "建议",
-        pinyin: "jiànyì",
-        meaning: "အကြံပြုသည်၊ အကြံပေးသည်"
+        chinese: "急诊室",
+        pinyin: "jízhěnshì",
+        meaning: "အရေးပေါ်အခန်း"
+    },
+
+    {
+        chinese: "症状",
+        pinyin: "zhèngzhuàng",
+        meaning: "ရောဂါ လက္ခဏာ"
+    },
+
+    {
+        chinese: "腹疼",
+        pinyin: "fù téng",
+        meaning: "ဗိုက် နာသည်"
+    },
+
+    {
+        chinese: "发热",
+        pinyin: "fā rè",
+        meaning: "အပူကြီးသည်"
+    },
+
+    {
+        chinese: "呕吐",
+        pinyin: "ǒutù",
+        meaning: "အော့အန်သည်"
+    },
+
+    {
+        chinese: "检查",
+        pinyin: "jiǎnchá",
+        meaning: "စစ်ဆေးသည်"
+    },
+
+    {
+        chinese: "患",
+        pinyin: "huàn",
+        meaning: "(ရောဂါ) ဖြစ်သည်"
+    },
+
+    {
+        chinese: "阑尾炎",
+        pinyin: "lánwěiyán",
+        meaning: "အူအတက် ရောင်ခြင်း"
+    },
+
+    {
+        chinese: "抽血",
+        pinyin: "chōu xuè",
+        meaning: "သွေးထုတ်သည်"
+    },
+
+    {
+        chinese: "化验",
+        pinyin: "huàyàn",
+        meaning: "စမ်းသပ် စစ်ဆေးသည်"
+    },
+
+    {
+        chinese: "单子",
+        pinyin: "dānzi",
+        meaning: "ဆေးစာ ပြစာ"
+    },
+
+    {
+        chinese: "收费处",
+        pinyin: "shōufèichù",
+        meaning: "ငွေလက်ခံ ကောင်တာ"
+    },
+
+    {
+        chinese: "交费",
+        pinyin: "jiāofèi",
+        meaning: "ပိုက်ဆံ ချေသည်"
+    },
+
+    {
+        chinese: "根据",
+        pinyin: "gēnjù",
+        meaning: "အတိုင်း၊ အရ အခြေအနေ"
+    },
+
+    {
+        chinese: "显示",
+        pinyin: "xiǎnshì",
+        meaning: "ပြသည်"
+    },
+
+    {
+        chinese: "慢性",
+        pinyin: "mànxìng",
+        meaning: "နာတာရှည်"
+    },
+
+    {
+        chinese: "痊愈",
+        pinyin: "quányù",
+        meaning: "(ရောဂါ) လုံးဝ နာလန်ပြန်ထူသည်။ နေပြန်ကောင်းသည်။"
+    },
+
+    {
+        chinese: "治疗",
+        pinyin: "zhìliáo",
+        meaning: "ကုသသည်"
+    },
+
+    {
+        chinese: "动手术",
+        pinyin: "dòng shǒushù",
+        meaning: "ခွဲစိတ်မှု လုပ်သည်"
+    },
+
+    {
+        chinese: "切除",
+        pinyin: "qiēchú",
+        meaning: "ဖြတ်ထုတ် ဖယ်ရှားသည်"
+    },
+
+    {
+        chinese: "危险性",
+        pinyin: "wēixiǎnxìng",
+        meaning: "အန္တရာယ် ရှိနိုင်ခြေ"
+    },
+
+    {
+        chinese: "尽量",
+        pinyin: "jǐnliàng",
+        meaning: "တတ်နိုင်သမျှ၊ ဖြစ်နိုင်သမျှ"
+    },
+
+    {
+        chinese: "发炎",
+        pinyin: "fā yán",
+        meaning: "(အနာ) ရောင်ရမ်းသည်"
+    },
+
+    {
+        chinese: "清淡",
+        pinyin: "qīngdàn",
+        meaning: "(အစား) ပေါ့ပါး အဆီမပါသော"
+    },
+
+    {
+        chinese: "消化",
+        pinyin: "xiāohuà",
+        meaning: "အစာချေသည်"
+    },
+
+    {
+        chinese: "流食",
+        pinyin: "liúshí",
+        meaning: "အရည် အစားအစာ (နူးညံ့ မျိုရလွယ်သည်)"
+    },
+
+    {
+        chinese: "油炸",
+        pinyin: "yóuzhá",
+        meaning: "ဆီကြော်"
+    },
+
+    {
+        chinese: "食品",
+        pinyin: "shípǐn",
+        meaning: "စားသောက်ကုန်နှင့် အစားအစာ"
+    },
+
+    {
+        chinese: "康复",
+        pinyin: "kāngfù",
+        meaning: "(အချိန်ကိုက်) နာလန်ပြန်ထူသည်။ နေပြန်ကောင်းသည်။"
+    },
+
+    {
+        chinese: "印象",
+        pinyin: "yìnxiàng",
+        meaning: "သဘောထား အမြင်၊ အမြင် စွဲကျန် (impression)"
+    },
+
+    {
+        chinese: "充满",
+        pinyin: "chōngmǎn",
+        meaning: "ပြည့်နှက်နေသည်"
     },
     {
-        chinese: "会议",
-        pinyin: "huìyì",
-        meaning: "အစည်းအဝေး"
+        chinese: "酒精",
+        pinyin: "jiǔjīng",
+        meaning: "အရက်ပြန်"
     },
+
     {
-        chinese: "失礼",
-        pinyin: "shīlǐ",
-        meaning: "ယဉ်ကျေးမှု မရှိ၊ မရိုမသေ ပြုသည်"
+        chinese: "消毒水",
+        pinyin: "xiāodúshuǐ",
+        meaning: "ပိုးသတ်ဆေး"
     },
+
     {
-        chinese: "直接",
-        pinyin: "zhíjiē",
-        meaning: "တိုက်ရိုက်"
+        chinese: "门诊部",
+        pinyin: "ménzhěnbù",
+        meaning: "ပြင်ပလူနာဌာန"
     },
+
     {
-        chinese: "生气",
-        pinyin: "shēngqì",
-        meaning: "စိတ်ဆိုးသည်"
+        chinese: "家属",
+        pinyin: "jiāshǔ",
+        meaning: "မိသားစုဝင်"
     },
+
     {
-        chinese: "原因",
-        pinyin: "yuányīn",
-        meaning: "အကြောင်းရင်း"
+        chinese: "生命",
+        pinyin: "shēngmìng",
+        meaning: "အသက်"
     },
+
     {
-        chinese: "不是……而是……",
-        pinyin: "bùshì...érshì...",
-        meaning: "A မဟုတ် B ဖြစ်တယ်"
+        chinese: "脆弱",
+        pinyin: "cuìruò",
+        meaning: "အားနည်းသော"
     },
+
     {
-        chinese: "随意",
-        pinyin: "suíyì",
-        meaning: "ဆန္ဒရှိသလို ပြုသလို ကြိုက်ရာပြု၊ စိတ်ကြိုက်"
-    },
-    {
-        chinese: "打断",
-        pinyin: "dǎduàn",
-        meaning: "ကြားဖြတ်သည်"
-    },
-    {
-        chinese: "发言",
-        pinyin: "fāyán",
-        meaning: "မိန့်ခွန်း၊ စကား"
-    },
-    {
-        chinese: "讲",
-        pinyin: "jiǎng",
-        meaning: "ပြောဆို ရှင်းပြသည်"
-    },
-    {
-        chinese: "发表",
-        pinyin: "fābiǎo",
-        meaning: "ကြေညာ ထုတ်ဖော်ပြောသည်၊ ထုတ်ဖော်ပြသည်"
-    },
-    {
-        chinese: "倾听",
-        pinyin: "qīngtīng",
-        meaning: "ရလွယ်အောင် နားထောင်သည်"
-    },
-    {
-        chinese: "坦白",
-        pinyin: "tǎnbái",
-        meaning: "ဝန်ခံသည်၊ ပွင့်လင်းသော"
-    },
-    {
-        chinese: "提出",
-        pinyin: "tíchū",
-        meaning: "တင်ပြပေးသည်၊ အဆိုပြုသည်"
-    },
-    {
-        chinese: "缺点",
-        pinyin: "quēdiǎn",
-        meaning: "အားနည်းချက်"
-    },
-    {
-        chinese: "相处",
-        pinyin: "xiāngchǔ",
-        meaning: "ပေါင်းသင်းနေထိုင်သည်"
-    },
-    {
-        chinese: "换位",
-        pinyin: "huànwèi",
-        meaning: "နေရာလဲသည်"
-    },
-    {
-        chinese: "思考",
-        pinyin: "sīkǎo",
-        meaning: "တွေးခေါ်၍ စဉ်းစားသည်"
-    },
-    {
-        chinese: "真诚",
-        pinyin: "zhēnchéng",
-        meaning: "ရိုးသား စိတ်မှန်သော၊ စိတ်ရင်း"
-    },
-    {
-        chinese: "道歉",
-        pinyin: "dàoqiàn",
-        meaning: "တောင်းပန် ရှင်းပြသည်"
+        chinese: "珍惜",
+        pinyin: "zhēnxī",
+        meaning: "အမြတ်တနိုး တန်ဖိုးထားသည်"
     }
+
 ];
