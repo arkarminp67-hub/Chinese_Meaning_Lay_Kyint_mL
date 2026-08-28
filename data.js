443135d9b117f9425048ca70e0b93b6c4aaf84a8
arkarminp67-hub <arkarmin.p67@rsu.ac.th>
2026-08-28 14:57:28 +0700

    Update data.js

--- data.js
+++ data.js
@@ -225,6 +225,111 @@ const WORD_DATA = [
         chinese: "珍惜",
         pinyin: "zhēnxī",
         meaning: "အမြတ်တနိုး တန်ဖိုးထားသည်"
+    },
+    
+    {
+        chinese: "建议",
+        pinyin: "jiànyì",
+        meaning: "အကြံပြုသည်၊ အကြံပေးသည်"
+    },
+    {
+        chinese: "会议",
+        pinyin: "huìyì",
+        meaning: "အစည်းအဝေး"
+    },
+    {
+        chinese: "失礼",
+        pinyin: "shīlǐ",
+        meaning: "ယဉ်ကျေးမှု မရှိ၊ မရိုမသေ ပြုသည်"
+    },
+    {
+        chinese: "直接",
+        pinyin: "zhíjiē",
+        meaning: "တိုက်ရိုက်"
+    },
+    {
+        chinese: "生气",
+        pinyin: "shēngqì",
+        meaning: "စိတ်ဆိုးသည်"
+    },
+    {
+        chinese: "原因",
+        pinyin: "yuányīn",
+        meaning: "အကြောင်းရင်း"
+    },
+    {
+        chinese: "不是……而是……",
+        pinyin: "bùshì...érshì...",
+        meaning: "A မဟုတ် B ဖြစ်တယ်"
+    },
+    {
+        chinese: "随意",
+        pinyin: "suíyì",
+        meaning: "ဆန္ဒရှိသလို ပြုသလို ကြိုက်ရာပြု၊ စိတ်ကြိုက်"
+    },
+    {
+        chinese: "打断",
+        pinyin: "dǎduàn",
+        meaning: "ကြားဖြတ်သည်"
+    },
+    {
+        chinese: "发言",
+        pinyin: "fāyán",
+        meaning: "မိန့်ခွန်း၊ စကား"
+    },
+    {
+        chinese: "讲",
+        pinyin: "jiǎng",
+        meaning: "ပြောဆို ရှင်းပြသည်"
+    },
+    {
+        chinese: "发表",
+        pinyin: "fābiǎo",
+        meaning: "ကြေညာ ထုတ်ဖော်ပြောသည်၊ ထုတ်ဖော်ပြသည်"
+    },
+    {
+        chinese: "倾听",
+        pinyin: "qīngtīng",
+        meaning: "ရလွယ်အောင် နားထောင်သည်"
+    },
+    {
+        chinese: "坦白",
+        pinyin: "tǎnbái",
+        meaning: "ဝန်ခံသည်၊ ပွင့်လင်းသော"
+    },
+    {
+        chinese: "提出",
+        pinyin: "tíchū",
+        meaning: "တင်ပြပေးသည်၊ အဆိုပြုသည်"
+    },
+    {
+        chinese: "缺点",
+        pinyin: "quēdiǎn",
+        meaning: "အားနည်းချက်"
+    },
+    {
+        chinese: "相处",
+        pinyin: "xiāngchǔ",
+        meaning: "ပေါင်းသင်းနေထိုင်သည်"
+    },
+    {
+        chinese: "换位",
+        pinyin: "huànwèi",
+        meaning: "နေရာလဲသည်"
+    },
+    {
+        chinese: "思考",
+        pinyin: "sīkǎo",
+        meaning: "တွေးခေါ်၍ စဉ်းစားသည်"
+    },
+    {
+        chinese: "真诚",
+        pinyin: "zhēnchéng",
+        meaning: "ရိုးသား စိတ်မှန်သော၊ စိတ်ရင်း"
+    },
+    {
+        chinese: "道歉",
+        pinyin: "dàoqiàn",
+        meaning: "တောင်းပန် ရှင်းပြသည်"
     }
-
 ];
